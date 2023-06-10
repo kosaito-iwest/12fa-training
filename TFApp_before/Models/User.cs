cb308bf2c410cd9bd974bf8bd412b906ddb2928f
kosaito-iwest <ko.saito@ts.infocom.co.jp>
2023-06-10 15:18:28 +0900

    DBマイグレーション

--- TFApp_before/Models/User.cs
+++ TFApp_before/Models/User.cs
@@ -5,5 +5,5 @@ public class User
     public string UserId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
-    public string Message2 { get; set; } = string2.Empty; 
+    public string Message { get; set; } = string.Empty; 
 }
